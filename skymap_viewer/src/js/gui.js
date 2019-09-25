8b21138cc551b31adbdc39d64f2879be709e6558
mrizzo25 <monriz5@aol.com>
2019-09-25 10:06:55 -0500

    removed redundant skymap dir

--- skymap_viewer/src/js/gui.js
+++
@@ -1,9 +0,0 @@
-function createUI(){
-
-      params.gui = new dat.GUI();
-      params.gui.add( params, 'event_name', params.GWEventOptions).onChange(function(){loadData();  drawAll();}).name("Event Name");
-      params.gui.add( params, 'ci', 15, 99).onChange(drawAll).name("CI");
-      params.gui.add( params, 'rd_axes').onChange(drawAll).name("Axes");	
-
-}
-
